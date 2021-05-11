1effaf2b876d7934319fd86cd5abd281d55cd74d
Salaiaung40 <salaihtunlin@hotnail.com>
2021-05-11 19:40:37 +0200

    Add Thymeleaf and list.html

---
+++ src/main/java/salai/springframework/webapp5simple/controller/BookController.java
@@ -0,0 +1,23 @@
+package salai.springframework.webapp5simple.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import salai.springframework.webapp5simple.repositories.BookRepository;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "books/list";
+    }
+}
